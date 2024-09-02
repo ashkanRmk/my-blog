0c9cd4ea88340750e4c8da695c16fbbd9ca420e6
Ashkan <a.rahmanikhorasani@alibaba.ir>
2024-09-02 18:32:41 +0330

    add sample endpoints

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://ashkanam.ir",
   integrations: [mdx(), sitemap(), tailwind()],
 });
